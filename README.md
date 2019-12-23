ac38c3dcd74747fea2479ec5c579ac6d898d2f46
dc2348 <dc2348@hanmail.net>
2019-12-23 17:03:51 +0900

    Add vue-news

--- README.md
+++ README.md
@@ -9,3 +9,7 @@
 ## :cherry_blossom:Reference
 - https://vuejs.org/v2/style-guide/, Vue 스타일 가이드
 - https://github.com/tastejs/hacker-news-pwas/blob/master/docs/api.md, 해커 뉴스 공식 사이트 주소
+
+## :cherry_blossom: 사이트 개발 순서
+1. 라우터 설계
+2. 컴포넌트
---
+++ vue-news/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <!-- Strongly Recommended -->
+    <!-- <ToolBar></ToolBar> -->
+    <!--  Essential -->
+    <tool-bar></tool-bar>
+    <router-view></router-view>
+    <!-- <NewView></NewView> -->
+  </div>
+</template>
+
+<script>
+import ToolBar from "./components/ToolBar.vue";
+export default {
+  components: {
+    ToolBar,
+  }
+}
+</script>
+
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+}
+</style>
